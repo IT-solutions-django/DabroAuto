cddb62e029601c0371e5e4ea6b3c2143474f7922
kirill <kirypleshkov@mail.ru>
2024-09-26 23:58:36 +0700

    Add service_info admin.py

--- src/apps/service_info/admin.py
+++ src/apps/service_info/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
 
-# Register your models here.
+from src.apps.service_info.models import (
+    SocialMedia,
+    StagesOfWork,
+    ContactInformation,
+    InformationAboutCompany,
+)
+
+
+@admin.register(SocialMedia)
+class SocialMediaAdmin(admin.ModelAdmin):
+    """Класс админ-панели социальной сети автосалона"""
+
+    search_fields = ("name",)
+
+
+@admin.register(StagesOfWork)
+class StagesOfWorkAdmin(admin.ModelAdmin):
+    """Класс админ-панели этапов работы автосалона"""
+
+    search_fields = ("name",)
+
+
+@admin.register(ContactInformation)
+class ContactInformationAdmin(admin.ModelAdmin):
+    """Класс админ-панели контактной информации автосалона"""
+
+    search_fields = ("name",)
+
+
+@admin.register(InformationAboutCompany)
+class InformationAboutCompanyAdmin(admin.ModelAdmin):
+    """Класс админ-панели с информацией об автосалоне"""
+
+    search_fields = ("block",)
+    fields = ("block", "content")
