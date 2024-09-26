682bc7b5b6e57a48c134fd26b07d6b21a9378b27
kirill <kirypleshkov@mail.ru>
2024-09-26 22:11:45 +0700

    Add comments to Car admin.py

--- src/apps/car/admin.py
+++ src/apps/car/admin.py
@@ -11,26 +11,36 @@ from src.apps.car.models import (
 
 @admin.register(CountryManufacturing)
 class CountryManufacturingAdmin(admin.ModelAdmin):
+    """Класс админ-панели страны создания автомобиля"""
+
     search_fields = ("name",)
 
 
 @admin.register(CarBrand)
 class CarBrandAdmin(admin.ModelAdmin):
+    """Класс админ-панели марки автомобиля"""
+
     search_fields = ("name",)
 
 
 @admin.register(CarModel)
 class CarModelAdmin(admin.ModelAdmin):
+    """Класс админ-панели модели автомобиля"""
+
     search_fields = ("name",)
 
 
 @admin.register(EngineType)
 class EngineTypeAdmin(admin.ModelAdmin):
+    """Класс админ-панели типа двигателя автомобиля"""
+
     search_fields = ("name",)
 
 
 @admin.register(Car)
 class CarAdmin(admin.ModelAdmin):
+    """Класс админ-панели автомобиля"""
+
     fields = (
         "brand",
         "model",
