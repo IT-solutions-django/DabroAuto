ae5554f586f55ce3443425b64492ca0554fbe6ea
kirill <kirypleshkov@mail.ru>
2024-09-26 20:59:55 +0700

    Add base model mixins

--- src/utils/model_mixins.py
+++ src/utils/model_mixins.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class NamingMixin(models.Model):
+class NameMixin(models.Model):
     """Mixin для добавления поля name (обязательное, уникальное)"""
 
     name = models.CharField(max_length=255, unique=True, verbose_name="Название")
