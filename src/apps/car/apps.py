eaf1a8a90e1da002952cfa8c195a49a546bbb33c
kirill <kirypleshkov@mail.ru>
2024-09-27 07:29:37 +0700

    Fix apps names

--- src/apps/car/apps.py
+++ src/apps/car/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class CarConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "src.apps.car"
-    verbose_name = "aвтомобиль"
+    verbose_name = "aвтомобили"
--- src/apps/review/apps.py
+++ src/apps/review/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class ReviewConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "src.apps.review"
-    verbose_name = "отзыв"
+    verbose_name = "отзывы"
