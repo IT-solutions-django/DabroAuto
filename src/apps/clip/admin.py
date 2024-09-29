59654ca7672b8e5128c55eb226d929433422c4ae
kirill <kirypleshkov@mail.ru>
2024-09-29 21:23:05 +0700

    Add Clip admin.py

--- src/apps/clip/admin.py
+++ src/apps/clip/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from src.apps.clip.models import Clip
+
+
+@admin.register(Clip)
+class ClipAdmin(admin.ModelAdmin):
+    search_fields = ("name", "url")
