7c188f6ea00a0680a1272a1d40b0a4fd473eea7c
kirill <kirypleshkov@mail.ru>
2024-09-30 22:34:10 +0700

    Add frontend layout

--- src/config/settings.py
+++ src/config/settings.py
@@ -95,10 +95,12 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = "static/"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
---
+++ src/static/js/main.js
@@ -0,0 +1,153 @@
+var swiper = new Swiper(".cars1 .mySwiperl", {
+    slidesPerView: 'auto',
+    spaceBetween: 16,
+    pagination: {
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".cars1 .swiper_next",
+      prevEl: ".cars1 .swiper_prev",
+    },
+    breakpoints: {
+      0: {
+        spaceBetween: 12,
+      },
+      768: {
+        spaceBetween: 16,
+      }
+    },
+});
+var swiper = new Swiper(".cars3 .mySwiperl", {
+    slidesPerView: 'auto',
+    spaceBetween: 16,
+    pagination: {
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".cars3 .swiper_next",
+      prevEl: ".cars3 .swiper_prev",
+    },
+    breakpoints: {
+      0: {
+        spaceBetween: 12,
+      },
+      768: {
+        spaceBetween: 16,
+      }
+    },
+});
+
+  var swiper = new Swiper(".cars2 .mySwipert", {
+    slidesPerView: 'auto',
+    spaceBetween: 16,
+    pagination: {
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".cars2 .swiper_next",
+      prevEl: ".cars2 .swiper_prev",
+    },
+  });
+
+
+var swiper = new Swiper(".mySwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+
+const track = document.querySelector('.logo-track');
+const logos = document.querySelectorAll('.logo-track img');
+
+// Duplicating logos for seamless scrolling
+track.innerHTML += track.innerHTML;
+
+track.style.width = `${logos.length * 200}px`; // Logolar kengligi (shu yerdagi raqamlarni logolar o'lchamiga qarab moslang)
+
+
+const track2 = document.querySelector('.logo-track');
+const logos2 = document.querySelectorAll('.logo-track img');
+
+// Duplicating logos for seamless scrolling
+track2.innerHTML += track2.innerHTML;
+
+track2.style.width = `${logos2.length * 200}px`
+
+const swiper1 = new Swiper('.clips_swiper', {
+  slidesPerView: 'auto',
+  spaceBetween: 16,
+  loop: true,
+  breakpoints: {
+    0: {
+      spaceBetween: 8,
+    },
+    768: {
+      spaceBetween: 16,
+    }
+  },
+});
+
+function addClassOnClick(clickableSelector, targetSelector, className) {
+  const clickableElements = document.querySelectorAll(clickableSelector);
+  const targetElement = document.querySelector(targetSelector);
+
+  clickableElements.forEach(element => {
+    element.addEventListener('click', function(e) {
+      e.preventDefault()
+      targetElement.classList.add(className);
+    });
+  });
+}
+
+function removeClassOnClick(clickableSelector, targetSelector, className) {
+  const clickableElements = document.querySelectorAll(clickableSelector);
+  const targetElement = document.querySelector(targetSelector);
+
+  clickableElements.forEach(element => {
+    element.addEventListener('click', function() {
+      targetElement.classList.remove(className);
+    });
+  });
+}
+
+
+addClassOnClick('.card_discoint a', '.modal_', 'show');
+addClassOnClick('.save_discount a ', '.modal_', 'show');
+removeClassOnClick('.modal_back', '.modal_', 'show');
+removeClassOnClick('.modal_ .close_modal', '.modal_', 'show');
+
+var swiper = new Swiper(".comment_swiper", {
+  slidesPerView: 'auto',
+  grid: {
+    rows: 2,
+  },
+  spaceBetween: 16,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    0: {
+      spaceBetween: 12,
+    },
+    768: {
+      spaceBetween: 16,
+    }
+  },
+});
+
+document.querySelector('.hamburger').addEventListener('click', function(){
+  document.querySelector('.header_menu').classList.toggle('open')
+})
+
+var telInputs = document.querySelectorAll('input[type=tel]');
+
+if (telInputs.length > 0) {
+    telInputs.forEach(function(input) {
+        var phoneMask = IMask(input, {
+            mask: "+{7} 000 000 00 00",
+        });
+    });
+}
