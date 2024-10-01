5d31555505dc3fea5a58bbe6608c82e0ccdb1bf0
kirill <kirypleshkov@mail.ru>
2024-10-01 12:54:21 +0700

    Start connecting DB to frontend

--- src/pages/home/views.py
+++ src/pages/home/views.py
@@ -21,17 +21,47 @@ class HomeView(TemplateView):
     def get_context_data(self, **kwargs) -> dict[str, Any]:
         context = super().get_context_data(id=id, **kwargs)
         context["title"] = "Главная"
-        context["cars"] = Car.objects.all().select_related(
-            "brand", "model", "engine_type", "country_manufacturing"
+        # context["cars"] = Car.objects.all().select_related(
+        #     "brand", "model", "engine_type", "country_manufacturing"
+        # )
+        # context["clips"] = Clip.objects.all()
+
+        context["phone_number_main"] = ContactInformation.objects.get(
+            name="Основной номер телефона",
+        ).content
+        context["years_on_market"] = InformationAboutCompany.objects.get(
+            block="Лет на рынке",
+        ).content
+        context["count_satisfied_customers"] = InformationAboutCompany.objects.get(
+            block="Довольных клиентов",
+        ).content
+        context["average_review"] = InformationAboutCompany.objects.get(
+            block="Средний рейтинг",
+        ).content
+        context["about_us"] = InformationAboutCompany.objects.get(
+            block="О нас",
+        ).content
+
+        context["tg_url"] = SocialMedia.objects.get(name="Телеграм-канал").url
+        context["vk_url"] = SocialMedia.objects.get(name="VK").url
+        context["inst_url"] = SocialMedia.objects.get(name="Instagram").url
+
+        context["phone_number"] = ContactInformation.objects.get(
+            name="Номер телефона",
+        ).content
+        context["whatsapp"] = ContactInformation.objects.get(
+            name="WhatsApp",
+        ).content
+        context["whatsapp_url"] = (
+            f"https://wa.me/{''.join(i for i in context["whatsapp"] if i.isdigit())}"
         )
+        context["address"] = ContactInformation.objects.get(
+            name="Адрес",
+        ).content
+
+        context["review_locations"] = ReviewLocation.objects.all()
+        context["stages_of_work"] = list(StagesOfWork.objects.all())
         context["reviews"] = Review.objects.all().select_related("author", "location")
         context["review_locations"] = ReviewLocation.objects.all()
-        context["social_media"] = SocialMedia.objects.all()
-        context["contact_information"] = ContactInformation.objects.all()
-        context["stages_of_work"] = StagesOfWork.objects.all()
-        context["information_about_company"] = InformationAboutCompany.objects.all()
-        context["clips"] = Clip.objects.all()
+
         return context
-
-
-9
