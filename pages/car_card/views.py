e8027baa8ad0d8219027b127b2fd522a58b21bf4
kirill <kirypleshkov@mail.ru>
2024-10-14 11:11:00 +0700

    Add questionnaire form to car card

--- pages/car_card/views.py
+++ pages/car_card/views.py
@@ -2,7 +2,9 @@ from typing import Any
 
 from django.views.generic import FormView, TemplateView
 
+from apps.service_info.models import SocialMedia, ContactInformation
 from business.catalog_parser import get_car_by_id
+from pages.home.forms import QuestionnaireForm
 
 
 class CarCardView(TemplateView):
@@ -20,5 +22,28 @@ class CarCardView(TemplateView):
 
         context["car"] = get_car_by_id(self.country, car_id)
         context["country"] = self.country
+
+        context["questionnaire_form"] = QuestionnaireForm
+
+        context["phone_number_main"] = ContactInformation.objects.get(
+            name="Основной номер телефона",
+        ).content
+
+        context["tg_url"] = SocialMedia.objects.get(name="Телеграм-канал").url
+        context["vk_url"] = SocialMedia.objects.get(name="VK").url
+        context["inst_url"] = SocialMedia.objects.get(name="Instagram").url
+
+        context["phone_number"] = ContactInformation.objects.get(
+            name="Номер телефона",
+        ).content
+        context["whatsapp"] = ContactInformation.objects.get(
+            name="WhatsApp",
+        ).content
+        context["whatsapp_url"] = (
+            f"https://wa.me/{''.join(i for i in context["whatsapp"] if i.isdigit())}"
+        )
+        context["address"] = ContactInformation.objects.get(
+            name="Адрес",
+        ).content
 
         return context
