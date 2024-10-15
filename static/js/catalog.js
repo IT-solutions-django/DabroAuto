9fdbcd3d4a2f845b0cad32bbbc69ee9b60685385
kirill <kirypleshkov@mail.ru>
2024-10-15 16:34:59 +0700

    Replace vanilla selects with dropdowns bootstrap

--- static/js/catalog.js
+++ static/js/catalog.js
@@ -1,8 +1,9 @@
 $(document).ready(function () {
-
     updateModels()
 
     updateClearButton()
+
+    connectSelects()
 
     $('#searchForm').submit(function () {
 
@@ -65,6 +66,7 @@ $(document).ready(function () {
 
                 // Обновляем контейнер с пагинацией
                 $('.pagination').html(paginationHtml);
+
                 updateClearButton();
             },
             error: function (response) {
@@ -74,40 +76,6 @@ $(document).ready(function () {
         });
         return false;
     });
-
-    $("[name='mark']").change(updateModels);
-
-    function updateModels() {
-        const markId = $("[name='mark']").val();
-        $.ajax({
-            url: `/models/?mark_id=${markId || -1}`,
-            type: 'GET',
-            success: function (data) {
-
-                const selectedModel = $('#id_model').val();
-                console.log(selectedModel)
-
-                // Очищаем текущий список моделей и добавляем первую опцию
-                $('#id_model').empty().append('<option value="" selected="">Модель авто</option>');
-
-                // Добавляем новые опции из данных
-                $.each(data, function (index, item) {
-                    $('#id_model').append('<option value="' + item.id + '">' + item.name + '</option>');
-                });
-
-                // Устанавливаем выбранную модель, если она присутствует в обновленных данных
-                if (data.some(item => Number(item.id) === Number(selectedModel))) {
-                    $('#id_model').val(selectedModel);
-                } else {
-                    $('#id_model').val(''); // Если модели нет, выбираем "Модель авто"
-                }
-
-            },
-            error: function (xhr, status, error) {
-                console.error('Ошибка AJAX:', error);
-            }
-        });
-    }
 
     function updateClearButton() {
         const newParams = $('#searchForm').serialize().split('&').filter(e => e.split('=')[0] != 'csrfmiddlewaretoken');
@@ -140,3 +108,82 @@ $(document).ready(function () {
         }
     }
 })
+
+const connectSelects = () => {
+    const selects = Array.from(document.querySelectorAll(".search-form select"));
+
+    selects.forEach(select => {
+        const dropdown = document.getElementById(`${select.name}-dropdown`);
+        const button = dropdown.querySelector('.catalog__filter-select--btn');
+        button.textContent = select.options[select.selectedIndex].text;
+    })
+};
+
+const setField = (field, value, text) => {
+    let dropdown = document.getElementById(`${field}-dropdown`);
+    var input = document.querySelector(`[name=${field}]`);
+    input.value = value;
+
+    // Выбор кнопки для изменения текста
+    let button = dropdown.querySelector('.catalog__filter-select--btn');
+
+    console.log(button)
+
+    if (value !== "") {
+        button.classList.add('font-weight-bold'); // Добавляем жирный шрифт для непустого значения
+    } else {
+        button.classList.remove('font-weight-bold'); // Убираем жирный шрифт для пустого значения
+    }
+
+    if (field === "mark") {
+        updateModels();
+    }
+
+    // Обновление текста кнопки
+    button.textContent = text;
+};
+
+function updateModels() {
+    const markId = $("[name='mark']").val();
+    $.ajax({
+        url: `/models/?mark_id=${markId || -1}`,
+        type: 'GET',
+        success: function (data) {
+
+            const selectedModel = $('#id_model').val();
+            console.log(selectedModel)
+
+            // Очищаем текущий список моделей и добавляем первую опцию
+            $('#id_model').empty().append('<option value="" selected="">Модель авто</option>');
+
+            // Добавляем новые опции из данных
+            $.each(data, function (index, item) {
+                $('#id_model').append('<option value="' + item.id + '">' + item.name + '</option>');
+            });
+
+            $('#model-dropdown .dropdown-menu').empty().append('<li><button type="button" onclick="setField(\'model\', \'None\', \'Модель авто\')" class="dropdown-item" href="#">Модель авто</button></li>');
+
+            // Добавляем новые опции из данных
+            $.each(data, function (index, item) {
+                $('#model-dropdown .dropdown-menu').append(`<li><button type="button" onclick="setField('model', '${item.id}', '${item.name}')" class="dropdown-item" href="#">${item.name}</button></li>`);
+            });
+
+            // Устанавливаем выбранную модель, если она присутствует в обновленных данных
+            if (data.some(item => Number(item.id) === Number(selectedModel))) {
+                $('#id_model').val(selectedModel);
+            } else {
+                $('#id_model').val(''); // Если модели нет, выбираем "Модель авто"
+            }
+
+            const select = document.querySelector("#id_model");
+
+            const dropdown = document.getElementById(`model-dropdown`);
+            const button = dropdown.querySelector('.catalog__filter-select--btn');
+            button.textContent = select.options[select.selectedIndex].text;
+
+        },
+        error: function (xhr, status, error) {
+            console.error('Ошибка AJAX:', error);
+        }
+    });
+}
