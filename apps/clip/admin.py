bf1485e66e84e148fa46e7e7e665aa4eb7604778
kirill <kirypleshkov@mail.ru>
2024-10-20 14:13:24 +0700

    Reorganize clips parsing settings

--- apps/clip/admin.py
+++ apps/clip/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from apps.clip.models import Clip
+from apps.clip.models import Clip, ClipPlatform
 
 
 @admin.register(Clip)
 class ClipAdmin(admin.ModelAdmin):
+    search_fields = ("name", "url")
+
+
+@admin.register(ClipPlatform)
+class ClipPlatformAdmin(admin.ModelAdmin):
     search_fields = ("name", "url")
